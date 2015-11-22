2b3380489ad55efb1f88f36952fbf118726ae350
Preston Richey <preston@40digits.com>
2015-11-21 18:53:05 -0600

    Init projects page

---
+++ routes/projects.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET projects page. */
+router.get('/', function (req, res, next) {
+  res.render('projects', {
+    title: 'projects'
+  });
+});
+
+router.get('/bots', function (req, res, next) {
+  res.send('bots');
+});
+
+router.get('/gbmf', function (req, res, next) {
+  res.send('gbmf');
+});
+
+module.exports = router;
