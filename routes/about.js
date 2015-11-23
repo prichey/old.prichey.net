3b5ed94215a5183a6a434efe0b9511ade2d5d87a
Preston Richey <preston@40digits.com>
2015-11-23 11:57:05 -0600

    Add commented out last.fm api url

--- routes/about.js
+++ routes/about.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+// http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=prichey&api_key=d2e1283a31bd5b2c450f7ed61cf9ab2e&format=json&limit=1
+
 /* GET about page. */
 router.get('/', function (req, res, next) {
   res.render('about', {
