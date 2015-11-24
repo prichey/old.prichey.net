6536d56659a267e3f8d5d9d76ecc20c90aab6e31
Preston Richey <preston@40digits.com>
2015-11-23 19:19:26 -0600

    Add request-promise; Pull in last.fm most recent track and display on about page

--- routes/about.js
+++ routes/about.js
@@ -1,13 +1,69 @@
 var express = require('express');
+var rp = require('request-promise');
 var router = express.Router();
 
 // http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=prichey&api_key=d2e1283a31bd5b2c450f7ed61cf9ab2e&format=json&limit=1
 
 /* GET about page. */
 router.get('/', function (req, res, next) {
-  res.render('about', {
-    title: 'about'
-  });
+    var lastfmOpts =  {
+        uri: 'http://ws.audioscrobbler.com/2.0',
+        qs: {
+            method: 'user.getrecenttracks',
+            user: 'prichey',
+            api_key: 'd2e1283a31bd5b2c450f7ed61cf9ab2e',
+            format: 'json',
+            limit: 1
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+
+    rp(lastfmOpts)
+        .then(function (resp) {
+            // console.log(resp);
+            var song = getLastSongFromResponse(resp);
+            res.render('about', {
+                title: 'about',
+                song: song
+            });
+        })
+        .catch(function (err) {
+            // API call failed...
+            res.render('about', {
+                title: 'about'
+            });
+        });
 });
+
+// use a promise
+function getLastSong() {
+    request('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=prichey&api_key=d2e1283a31bd5b2c450f7ed61cf9ab2e&format=json&limit=1', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var body = JSON.parse(body);
+            console.log(body.recenttracks);
+            var returnSong = {
+                'title': body.recenttracks.track[0].name,
+                'artist': body.recenttracks.track[0].artist
+            };
+            console.log('returning', returnSong);
+            return returnSong;
+        } else {
+            console.log('returning nothing');
+            return {};
+        }
+    });
+    return body;
+}
+
+function getLastSongFromResponse(resp) {
+    var returnObj = {};
+    if (!!resp.recenttracks.track[0]['@attr'].nowplaying) {
+        returnObj = {
+            'title': resp.recenttracks.track[0].name,
+            'artist': resp.recenttracks.track[0].artist['#text']
+        };
+    }
+    return returnObj;
+}
 
 module.exports = router;
